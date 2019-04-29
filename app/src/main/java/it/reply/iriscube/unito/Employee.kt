d33caed7572f4081d3f369cb292a50598fd998a3
Alessandro Conti <a.conti@reply.it>
2019-04-29 22:23:39 +0200

    employee list

--- app/src/main/java/it/reply/iriscube/unito/Employee.kt
+++ app/src/main/java/it/reply/iriscube/unito/Employee.kt
@@ -2,6 +2,16 @@ package it.reply.iriscube.unito
 
 data class Employee(var id: Int, var name: String, var sal: Int) {
 
+    companion object {
+        fun getEmpList(): List<Employee> {
+            val emps = ArrayList<Employee>()
+            emps.add(Employee(1, "Yoda", 15000))
+            emps.add(Employee(2, "Luke", 25000))
+            emps.add(Employee(3, "Anakin", 5000))
+            return emps
+        }
+    }
+
     fun getEmpNameWithHighestSalary(): String {
         /**
          * logic to get Highest paid employee
