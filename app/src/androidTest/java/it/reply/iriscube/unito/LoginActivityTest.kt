8e7616002581aae2aaab98c7f0d3a190c325b90e
Marco Balanzino <m.balanzino@reply.it>
2019-05-01 21:32:34 +0200

    Intent exercise

--- app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
+++ app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
@@ -1,19 +1,5 @@
 package it.reply.iriscube.unito
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.*
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.intent.Intents.intended
-import android.support.test.espresso.intent.matcher.IntentMatchers.hasData
-import android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
-import android.support.test.espresso.intent.rule.IntentsTestRule
-import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.rule.ActivityTestRule
-import it.reply.iriscube.unito.activity.LoginActivity
-import it.reply.iriscube.unito.activity.WelcomeActivity
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.not
-import org.junit.Rule
 import org.junit.Test
 
 /**
@@ -22,16 +8,12 @@ import org.junit.Test
 
 class LoginActivityTest {
 
-    @get:Rule
-    val mIntentrule = IntentsTestRule<LoginActivity>(LoginActivity::class.java)
+
+    //TODO: Define the correct rule to check for the intent
+
 
     @Test
     fun successfulLoginShouldOpenMainScreen() {
-        onView(withId(R.id.userNameTextView)).perform(typeText("Marco"), closeSoftKeyboard())
-        onView(withId(R.id.passwordTextView)).perform(typeText("123456789"), closeSoftKeyboard())
-        onView(withId(R.id.loginButton)).perform(click())
-        intended(allOf(
-            hasExtraWithKey(LoginActivity.USERNAME)
-        ))
+        //TODO: Check that the login is successful and that the WelcomeActivity intent starts correctly
     }
 }
